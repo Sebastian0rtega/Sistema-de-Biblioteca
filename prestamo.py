c5777e0aba1c6d9c651d0feeb9f78b7272b8070a
Sebastian Ortega <sebastian.elias.ortega@gmail.com>
2026-02-12 11:37:13 -0300

    update clase pretamo para establecer fechas

---
+++ prestamo.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class Prestamo:
+    def __init__(self,libro,usuario):
+        self.libro = libro
+        self.usurio = usuario
+        self.fecha_prestamo = datetime.now()
+        self.fecha_devolucion = datetime.now()
+
+    def cerrar_pretamo(self):
+        self.fecha_devolucion= datetime.now()
